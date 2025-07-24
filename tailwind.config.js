f3ff26d0d6dc2f9012a86d9fe9bb2f2fe7cd2fd9
Misterreme <nunezemmanuel107@gmail.com>
2025-07-24 00:57:11 -0400

    DEMO:1.0.0 FASE: MVP

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'w-[10%]',
+    'w-[20%]',
+    'w-[30%]',
+    'w-[40%]',
+    'w-[50%]',
+    'w-[60%]',
+    'w-[70%]',
+    'w-[80%]',
+    'w-[90%]',
+    'w-[100%]',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
